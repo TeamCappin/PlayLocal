ef6bd346a736f00414bbe57d21a1be91909fba74
Hudson Lu <131294600+HudsonLu@users.noreply.github.com>
2025-12-26 13:42:49 -0500

    Adjusted CI workflows for UTs

--- playlocal/frontend/jest.config.js
+++ playlocal/frontend/jest.config.js
@@ -1,5 +1,21 @@
-module.exports = {
-  testEnvironment: "jest-environment-jsdom",
+/* eslint-disable @typescript-eslint/no-require-imports */
+// jest.config.js
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({ dir: "./" });
+
+const customJestConfig = {
+  testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  testMatch: ["<rootDir>/test/**/*.(test|spec).(ts|tsx|js|jsx)"],
+
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "text-summary", "clover", "json"],
+
+  reporters: [
+    "default",
+    ["jest-junit", { outputDirectory: "coverage/test-results", outputName: "junit.xml" }],
+  ],
 };
+
+module.exports = createJestConfig(customJestConfig);
