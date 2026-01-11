f4941ea8eea047031f2a8265f0b1e7ee6151b8c6
Melissa Rahman <melissa.rahman2002@gmail.com>
2026-01-10 21:54:15 -0500

    WIP: Added attendance,  no shows, and not marked UI element cards

--- playlocal/frontend/components/RsvpRoster.tsx
+++ playlocal/frontend/components/RsvpRoster.tsx
@@ -1,7 +1,6 @@
-import { BadgeAlert } from 'lucide-react';
 import Link from 'next/link';
-import { Button, buttonVariants } from './ui/button';
-import { CircleCheckBig, CircleX } from 'lucide-react';
+import { Button } from './ui/button';
+import { CircleCheckBig, CircleX, TriangleAlert } from 'lucide-react';
 
 export function RsvpRoster() {
 
@@ -17,7 +16,9 @@ export function RsvpRoster() {
     gameStartTime: new Date().toISOString(),
     gameLocationId: 'aaaa1111-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
     locationName: 'Parc Jary Courts', //location
-    uiPlayerCount: 10,
+    uiPlayerCountAttended: 0,
+    uiPlayerCountNoShows: 0,
+    uiPlayerCountTotal: 8,
     participantsList: ['get participants ids based on gameId', 'get name based on participation_id -> user_id -> display_name', 'get game role based on sport_id + requested_position_role_id -> name'], //game_participation
     participantPositionRole: 'Guard',
     participantDefaultIntensity: 'Intermediate', //'game_participation_id -> user_id -> default_intensity',
@@ -29,18 +30,61 @@ export function RsvpRoster() {
     <div className="min-h-screen bg-gray-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="py-8">
+          <div className="grid md:grid-cols-3 gap-4 mb-8">
+
+            <div className='flex bg-white rounded-xl border border-gray-200 px-6 py-2 gap-4 items-center'>
+              <div>
+                <CircleCheckBig className="w-10 h-10 text-emerald-600" />
+              </div>
+              <div className='flex-col'>
+                <div className="text-3xl text-gray-900">
+                  {rsvpRosterInfo.uiPlayerCountAttended}
+                </div>
+                <div className='text-gray-600'>
+                  Attended
+                </div>
+              </div>
+            </div>
+
+            <div className='flex bg-white rounded-xl border border-gray-200 px-6 py-2 gap-4 items-center'>
+              <div>
+                <CircleX className="w-10 h-10 text-red-600" />
+              </div>
+              <div className='flex-col'>
+                <div className="text-3xl text-gray-900">
+                  {rsvpRosterInfo.uiPlayerCountNoShows}
+                </div>
+                <div className='text-gray-600'>
+                  No Shows
+                </div>
+              </div>
+            </div>
+
+            <div className='flex bg-white rounded-xl border border-gray-200 px-6 py-2 gap-4 items-center'>
+              <div>
+                <TriangleAlert className="w-10 h-10 text-gray-500" />
+              </div>
+              <div className='flex-col'>
+                <div className="text-3xl text-gray-900">
+                  {rsvpRosterInfo.uiParticipantsCountLeftToMark}
+                </div>
+                <div className='text-gray-600'>
+                  Not Marked
+                </div>
+              </div>
+            </div>
+
+          </div>
           <div className="grid">
             <div className="bg-white rounded-xl border border-gray-200 p-6">
-
               <div className="flex-col">
                 <div className='text-3xl text-gray-900 mb-2'>
-                  RSVP Roster ({rsvpRosterInfo.uiPlayerCount} players)
+                  RSVP Roster ({rsvpRosterInfo.uiPlayerCountTotal} players)
                 </div>
                 <div className='text-gray-600 pb-6'>
                   Mark each player as attended or no-show
                 </div>
               </div>
-
               <div className="space-y-3">
                 <div className="flex items-center justify-between p-4 bg-gray-50 rounded-lg transition-colors">
 
@@ -66,7 +110,7 @@ export function RsvpRoster() {
                       <div>
                         <Button className='border-2 hover:bg-gray-200 transition-colors'>
                           <CircleX />
-                          No-show
+                          No show
                         </Button>
                       </div>
                     </div>
@@ -96,7 +140,7 @@ export function RsvpRoster() {
                       <div>
                         <Button className='border-2 hover:bg-gray-200 transition-colors'>
                           <CircleX />
-                          No-show
+                          No show
                         </Button>
                       </div>
                     </div>
@@ -104,11 +148,10 @@ export function RsvpRoster() {
                 </div>
               </div>
             </div>
-
           </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 }
 
