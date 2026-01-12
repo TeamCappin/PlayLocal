14dcbb4e6c46349b620df1c83ea06802a2b11490
Melissa Rahman <melissa.rahman2002@gmail.com>
2026-01-11 20:37:43 -0500

    database query to retrieve the past games for an organizer and display actions required notifications

--- playlocal/frontend/components/sub-components/ActionsRequired.tsx
+++ playlocal/frontend/components/sub-components/ActionsRequired.tsx
@@ -1,28 +1,48 @@
 import { CircleAlert } from 'lucide-react';
 import Link from 'next/link';
 
+import { useAuth } from '@/context/AuthContext';
+import { useRouter } from 'next/navigation';
+
+import { usePastGamesByUser } from '@/hooks/useGames';
+import { tr } from 'date-fns/locale';
+
 export function ActionsRequired() {
-  const gamesLeftToMarkAttendanceFor = [
-    {
-      id: '1',
-      createdBy: 'user123',
-      title: '5v5 Basketball Pickup',
-      sport: 'Basketball',
-      date: 'Dec 18, 2024',
-    },
-    {
-      id: '2',
-      title: 'Friendly Soccer Match',
-      sport: 'Soccer',
-      date: 'Dec 15, 2024',
-    },
-    {
-      id: '3',
-      title: 'Ultimate Frisbee Pickup',
-      sport: 'Ultimate Frisbee',
-      date: 'Dec 10, 2024',
-    },
-  ];
+  const navigate = useRouter();
+  const { isAuthenticated, user } = useAuth();
+
+  const { games, isLoading, error, refetch } = usePastGamesByUser(user?.userId || '');
+  console.log('Past games fetched from API:', games);
+
+  // const gamesLeftToMarkAttendanceFor = [
+  //   {
+  //     id: '1',
+  //     createdBy: 'user123',
+  //     title: '5v5 Basketball Pickup',
+  //     sport: 'Basketball',
+  //     date: 'Dec 18, 2024',
+  //   },
+  //   {
+  //     id: '2',
+  //     title: 'Friendly Soccer Match',
+  //     sport: 'Soccer',
+  //     date: 'Dec 15, 2024',
+  //   },
+  //   {
+  //     id: '3',
+  //     title: 'Ultimate Frisbee Pickup',
+  //     sport: 'Ultimate Frisbee',
+  //     date: 'Dec 10, 2024',
+  //   },
+  // ];
+
+  const handleClickConfirmAttendance = () => {
+    if (!isAuthenticated) {
+      navigate.push('/login');
+      return;
+    }
+    console.log("Confirm Attendance button clicked");
+  }
 
   return (
     <div>
@@ -32,26 +52,26 @@ export function ActionsRequired() {
           <h2 className="text-xl text-gray-900 leading-none">Actions Required</h2>
         </span>
         <div className="space-y-3">
-          {gamesLeftToMarkAttendanceFor.map((game) => (
+          {games.map((game) => (
             <div
-              key={game.title}
+              key={game.gameId}
               className="flex items-center justify-between p-4 bg-gray-50 rounded-lg hover:bg-gray-100 transition-colors"
             >
               <div className="flex items-center gap-4">
                 <div>
                   <div className="text-gray-900">{game.title}</div>
                   <div className="text-sm text-gray-600">
-                    {game.date}
+                    {game.startTime}
                   </div>
                 </div>
               </div>
               <div className="flex items-center gap-4">
                 <Link
-                  key={game.id}
+                  key={game.gameId}
                   href={`/rsvpRoster`}
                   className="flex items-center justify-between p-4 bg-gray-50 rounded-lg hover:bg-gray-100 transition-colors"
                 >
-                  <button className="px-4 py-2 bg-gradient-to-br from-emerald-500 to-teal-500 rounded-lg flex items-center justify-center text-white hover:text-emerald-700">
+                  <button onClick={handleClickConfirmAttendance} className="px-4 py-2 bg-gradient-to-br from-emerald-500 to-teal-500 rounded-lg flex items-center justify-center text-white hover:text-emerald-700">
                     Confirm Attendance
                   </button>
                 </Link>
