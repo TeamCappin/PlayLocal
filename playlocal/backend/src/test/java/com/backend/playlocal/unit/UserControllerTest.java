e46833d3e9f3da5569e3d88c2cf91d27d186e56d
Yuce-Cybr <92345443+Yuce-Cybr@users.noreply.github.com>
2026-02-18 23:01:11 -0500

    import java.util.Map;

--- playlocal/backend/src/test/java/com/backend/playlocal/unit/UserControllerTest.java
+++ playlocal/backend/src/test/java/com/backend/playlocal/unit/UserControllerTest.java
@@ -20,6 +20,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
